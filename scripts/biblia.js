47f1c34794957cb3aa3f907ab32c623f1bd3d534
Gleider Ribeiro Costa <gleiderr@gmail.com>
2017-10-23 21:25:16 -0200

    Habilitado acesso dinâmico a todo texto bíblico

--- scripts/biblia.js
+++ scripts/biblia.js
@@ -1,106 +1,119 @@
+const kja_books = {
+    '1Ch': { livro: '1º Crônicas', nCapitulos: 29},
+    '1Co': { livro: '1ª Corintios', nCapitulos: 16},
+    '1Jo': { livro: '1ª João', nCapitulos: 5},
+    '1Ki': { livro: '1º Reis', nCapitulos: 22},
+    '1Pe': { livro: '1ª Pedro', nCapitulos: 5},
+    '1Sa': { livro: '1º Samuel', nCapitulos: 31},
+    '1Th': { livro: '1ª Tessalonicenses', nCapitulos: 5},
+    '1Ti': { livro: '1ª Timóteo', nCapitulos: 6},
+    '2Ch': { livro: '2º Crônicas', nCapitulos: 36},
+    '2Co': { livro: '2ª Corintios', nCapitulos: 13},
+    '2Jo': { livro: '2ª João', nCapitulos: 1},
+    '2Ki': { livro: '2º Reis', nCapitulos: 25},
+    '2Pe': { livro: '2ª Pedro', nCapitulos: 3},
+    '2Sa': { livro: '2º Samuel', nCapitulos: 24},
+    '2Th': { livro: '2ª Tessalonicenses', nCapitulos: 3},
+    '2Ti': { livro: '2ª Timóteo', nCapitulos: 4},
+    '3Jo': { livro: '3ª João', nCapitulos: 1},
+    'Act': { livro: 'Atos', nCapitulos: 28},
+    'Amo': { livro: 'Amós', nCapitulos: 9},
+    'Col': { livro: 'Colossences', nCapitulos: 4},
+    'Dan': { livro: 'Daniel', nCapitulos: 12},
+    'Deu': { livro: 'Deutoronômio', nCapitulos: 34},
+    'Ecc': { livro: 'Eclesiastes', nCapitulos: 12},
+    'Eph': { livro: 'Efésios', nCapitulos: 6},
+    'Est': { livro: 'Ester', nCapitulos: 10},
+    'Exo': { livro: 'Êxodo', nCapitulos: 40},
+    'Eze': { livro: 'Ezequiel', nCapitulos: 48},
+    'Ezr': { livro: 'Esdras', nCapitulos: 10},
+    'Gal': { livro: 'Gálatas', nCapitulos: 6},
+    'Gen': { livro: 'Gênesis', nCapitulos: 50},
+    'Hab': { livro: 'Habacuque', nCapitulos: 3},
+    'Hag': { livro: 'Ageu', nCapitulos: 2},
+    'Heb': { livro: 'Hebreus', nCapitulos: 13},
+    'Hos': { livro: 'Oséias', nCapitulos: 14},
+    'Isa': { livro: 'Isaías', nCapitulos: 66},
+    'Jam': { livro: 'Tiago', nCapitulos: 5},
+    'Jdg': { livro: 'Juízes', nCapitulos: 21},
+    'Jer': { livro: 'Jeremias', nCapitulos: 52},
+    'Job': { livro: 'Jó', nCapitulos: 42},
+    'Joe': { livro: 'Joel', nCapitulos: 3},
+    'Joh': { livro: 'João', nCapitulos: 21},
+    'Jon': { livro: 'Jonas', nCapitulos: 4},
+    'Jos': { livro: 'Josué', nCapitulos: 24},
+    'Jud': { livro: 'Judas', nCapitulos: 1},
+    'Lam': { livro: 'Lamentações', nCapitulos: 5},
+    'Lev': { livro: 'Levítico', nCapitulos: 27},
+    'Luk': { livro: 'Lucas', nCapitulos: 24},
+    'Mal': { livro: 'Malaquias', nCapitulos: 4},
+    'Mar': { livro: 'Marcos', nCapitulos: 16},
+    'Mat': { livro: 'Mateus', nCapitulos: 28},
+    'Mic': { livro: 'Miquéias', nCapitulos: 7},
+    'Nah': { livro: 'Naum', nCapitulos: 3},
+    'Neh': { livro: 'Neemias', nCapitulos: 13},
+    'Num': { livro: 'Números', nCapitulos: 18},
+    'Oba': { livro: 'Obadias', nCapitulos: 1},
+    'Phi': { livro: 'Filipenses', nCapitulos: 4},
+    'Phm': { livro: 'Filemom', nCapitulos: 1},
+    'Pro': { livro: 'Provérbios', nCapitulos: 31},
+    'Psa': { livro: 'Salmos', nCapitulos: 150},
+    'Rev': { livro: 'Apocalípse', nCapitulos: 22},
+    'Rom': { livro: 'Romanos', nCapitulos: 16},
+    'Rth': { livro: 'Rute', nCapitulos: 4},
+    'Son': { livro: 'Cantares', nCapitulos: 8},
+    'Tit': { livro: 'Tito', nCapitulos: 3},
+    'Zec': { livro: 'Zacarias', nCapitulos: 14},
+    'Zep': { livro: 'Sofonias', nCapitulos: 3},
+};
+
+
 $(document).ready(function() {
-    var kja_books = [
-        { sigla: '1Ch', livro: '1º Crônicas'},
-        { sigla: '1Co', livro: '1ª Corintios'},
-        { sigla: '1Jo', livro: '1ª João'},
-        { sigla: '1Ki', livro: '1º Reis'},
-        { sigla: '1Pe', livro: '1ª Pedro'},
-        { sigla: '1Sa', livro: '1º Samuel'},
-        { sigla: '1Th', livro: '1ª Tessalonicenses'},
-        { sigla: '1Ti', livro: '1ª Timóteo'},
-        { sigla: '2Ch', livro: '2º Crônicas'},
-        { sigla: '2Co', livro: '2ª Corintios'},
-        { sigla: '2Jo', livro: '2ª João'},
-        { sigla: '2Ki', livro: '2º Reis'},
-        { sigla: '2Pe', livro: '2ª Pedro'},
-        { sigla: '2Sa', livro: '2º Samuel'},
-        { sigla: '2Th', livro: '2ª Tessalonicenses'},
-        { sigla: '2Ti', livro: '2ª Timóteo'},
-        { sigla: '3Jo', livro: '3ª João'},
-        { sigla: 'Act', livro: 'Atos'},
-        { sigla: 'Amo', livro: 'Amós'},
-        { sigla: 'Col', livro: 'Colossences'},
-        { sigla: 'Dan', livro: 'Daniel'},
-        { sigla: 'Deu', livro: 'Deutoronômio'},
-        { sigla: 'Ecc', livro: 'Eclesiastes'},
-        { sigla: 'Eph', livro: 'Eph'},
-        { sigla: 'Est', livro: 'Est'},
-        { sigla: 'Exo', livro: 'Exo'},
-        { sigla: 'Eze', livro: 'Eze'},
-        { sigla: 'Ezr', livro: 'Ezr'},
-        { sigla: 'Gal', livro: 'Gal'},
-        { sigla: 'Gen', livro: 'Gen'},
-        { sigla: 'Hab', livro: 'Hab'},
-        { sigla: 'Hag', livro: 'Hag'},
-        { sigla: 'Heb', livro: 'Heb'},
-        { sigla: 'Hos', livro: 'Hos'},
-        { sigla: 'Isa', livro: 'Isa'},
-        { sigla: 'Jam', livro: 'Jam'},
-        { sigla: 'Jdg', livro: 'Jdg'},
-        { sigla: 'Jer', livro: 'Jer'},
-        { sigla: 'Job', livro: 'Job'},
-        { sigla: 'Joe', livro: 'Joe'},
-        { sigla: 'Joh', livro: 'Joh'},
-        { sigla: 'Jon', livro: 'Jon'},
-        { sigla: 'Jos', livro: 'Jos'},
-        { sigla: 'Jud', livro: 'Jud'},
-        { sigla: 'Lam', livro: 'Lam'},
-        { sigla: 'Lev', livro: 'Lev'},
-        { sigla: 'Luk', livro: 'Luk'},
-        { sigla: 'Mal', livro: 'Mal'},
-        { sigla: 'Mar', livro: 'Mar'},
-        { sigla: 'Mat', livro: 'Mat'},
-        { sigla: 'Mic', livro: 'Mic'},
-        { sigla: 'Nah', livro: 'Nah'},
-        { sigla: 'Neh', livro: 'Neh'},
-        { sigla: 'Num', livro: 'Num'},
-        { sigla: 'Oba', livro: 'Oba'},
-        { sigla: 'Phi', livro: 'Phi'},
-        { sigla: 'Phm', livro: 'Phm'},
-        { sigla: 'Pro', livro: 'Pro'},
-        { sigla: 'Psa', livro: 'Psa'},
-        { sigla: 'Rev', livro: 'Rev'},
-        { sigla: 'Rom', livro: 'Rom'},
-        { sigla: 'Rth', livro: 'Rth'},
-        { sigla: 'Son', livro: 'Son'},
-        { sigla: 'Tit', livro: 'Tit'},
-        { sigla: 'Zec', livro: 'Zec'},
-        { sigla: 'Zep', livro: 'Zep'},
-    ];
+    const ordem = [ 'Gen', 'Exo', 'Lev', 'Num', 'Deu', 'Jos', 'Jdg', 'Rth', '1Sa', '2Sa', '1Ki', '2Ki', '1Ch', '2Ch', 'Ezr', 'Neh', 'Est', 'Job', 'Psa', 'Pro', 'Ecc', 'Son', 'Isa', 'Jer', 'Lam', 'Eze',  'Dan', 'Hos', 'Joe', 'Amo', 'Oba', 'Jon', 'Mic', 'Nah', 'Hab', 'Zep', 'Hag', 'Zec', 'Mal', 'Mat', 'Mar', 'Luk', 'Joh', 'Act', 'Rom', '1Co', '2Co',  'Gal', 'Eph', 'Phi', 'Col', '1Th', '2Th', '1Ti', '2Ti', 'Tit', 'Phm', 'Heb', 'Jam', '1Pe', '2Pe', '1Jo', '2Jo', '3Jo', 'Jud', 'Rev'];
 
-    $('body').append('<form onsubmit="return false;"></form>');
-    for(let i = 0; i < kja_books.length; i++) {
-        let span = $('<span></span>')
-                    .attr('id', kja_books[i].sigla)
-                    .text(kja_books[i].livro);
-        for(let j = 1; j < 18; j++) {
-            let button = $('<button></button>')
-                            .css({'padding': '0px'})
-                            .text(j)
-                            .on('click', function(e) {
-                                open_bible(kja_books[i].sigla, j);
-                            });
+    for(let i = 0; i < ordem.length; i++) {
+        let sigla = ordem[i];
+        let opt = $('<option></option>')
+                    .attr('value', sigla)
+                    .text(kja_books[sigla].livro);
 
-
-            span.append(button);
-        }
-
-        $('form').append(span);
+        $('select').append(opt);
     }
+
+    $('select').on('change', function() {
+        showChapters(this.value);
+    });
+
+    showChapters($('select').val())
 });
 
+function showChapters(sigla) {
+    let capitulos = $('#capitulos');
+    capitulos.empty();
+    
+    let n = kja_books[sigla].nCapitulos;
+    for(let i = 1; i <= n; i++) {
+        let button = $('<button></button>')
+                        .text(i)
+                        .on('click', function(e) {
+                            open_bible(sigla, i);
+                        });
+        capitulos.append(button);
+    }
+}
+
 function open_bible(sigla, capitulo) {
-    console.log(sigla, capitulo);
     firebase.database().ref('/biblia/kja/' + sigla + '/' + capitulo).once('value').then(
         function(snapshot) {
-            snapshot.forEach(function(ver) {
-                console.log(ver.key + ' - ' + ver.val());
-                let p = $('<p></p>').text(ver.key + ' - ' + ver.val());
-                $('body').append(p);
+            let texto = $('#texto').empty();
+
+            snapshot.forEach(function(versiculo) {
+                let p = $('<p></p>').html(versiculo.key + ' - ' + versiculo.val());
+                texto.append(p);
             });
         },
         function(a, b, c) {
-            console.log('Deu ruim');
+            console.log('Deu ruim', a, b, c);
         }
-        );
+    );
 }
